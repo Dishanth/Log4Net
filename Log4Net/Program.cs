451cd41e78feee7da77d40bf26ce0b2c9548b014
Dishanth Nagarajaiah <ndishanthn@gmail.com>
2019-09-16 11:00:35 -0400

    Added logging Code

---
+++ Log4Net/Program.cs
@@ -0,0 +1,23 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Log4Net
+{
+    class Program
+    {
+        public static readonly ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+        static void Main(string[] args)
+        {
+            log.Info("Display info msg");
+            log.Debug("Display debug msg");
+            log.Warn("Display warn msg");
+            log.Error("Display Error msg");
+            log.Fatal("Display fatal msg");
+        }
+    }
+}
